103d95530ea86cf9a3d37b8975fe06a08d9677df
blooge4231 <57263072+blooge4231@users.noreply.github.com>
2020-10-01 21:30:25 -0400

    Create CCC'18 J2.java

---
+++ DMOJ/CCC/CCC'18 J2.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class parking {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+	Scanner sc = new Scanner(System.in);
+	
+	int n = sc.nextInt();
+	
+	String s = sc.next();
+	sc.nextLine();
+	String s1 = sc.next();
+	
+	int count = 0;
+	
+	for(int i = 0; i<n; i++) {
+		if(s.charAt(i)=='C'&& s1.charAt(i)=='C') {
+			count++;
+		}
+	}
+	
+	System.out.println(count);
+	
+}
+}

f6ff864b03aac9df587ecd078e125783f42ef2eb
blooge4231 <57263072+blooge4231@users.noreply.github.com>
2021-01-18 21:22:10 -0500

    Squares

---
+++ DMOJ/CCC/CCC'04 J1.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+    int a;
+    cin>>a;
+    int i;
+    for(i = 1; i*i <=a; i++){
+    }
+    cout<<"The largest square has side length "<<i-1<<"."<<endl;
+    
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    solve();
+    return 0;
+}

86694976875f65df947fbbc998497ba0513e439d
blooge4231 <57263072+blooge4231@users.noreply.github.com>
2021-01-18 21:09:24 -0500

    A Plus B

---
+++ DMOJ/Other/APlusB.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+    int n;
+    cin>>n;
+    for(int i = 0; i<n;i++){
+        int x, y;
+        cin>>x>>y;
+        cout<<x+y<<endl;
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    solve();
+    return 0;
+}

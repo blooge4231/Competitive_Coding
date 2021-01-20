52d02397be0180a025177df69a64ff6b0fbc02ed
blooge4231 <57263072+blooge4231@users.noreply.github.com>
2021-01-20 15:55:28 -0500

    Telemarketer or not?

---
+++ DMOJ/CCC/CCC'18 J1.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    
+    int a, b, c, d;
+    cin>>a>>b>>c>>d;
+
+    if((a!=9&&a!=8)||(d!=8&&d!=9)||b!=c){
+        cout<<"answer";
+    } else {
+        cout<<"ignore";
+    }
+
+
+    
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    solve();
+    return 0;
+}

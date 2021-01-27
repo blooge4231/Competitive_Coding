a8e14f928608a1cb55bf32e4788c285815b9485f
blooge4231 <57263072+blooge4231@users.noreply.github.com>
2021-01-27 14:51:17 -0500

    Update Bob's Rocks.cpp

--- DMOJ/Other/Bob's Rocks.cpp
+++ DMOJ/Other/Bob's Rocks.cpp
@@ -4,40 +4,22 @@ using namespace std;
 void solve(){
     int n;
     cin>>n;
-    vector<int> rocks;
-
+    map<int,int> freq;
     for(int i = 0 ; i < n; i++){
         int s;
         cin>>s;
-        rocks.push_back(s);
+        freq[s]++;
     }
 
-    sort(rocks.begin(), rocks.end());
 
     int max = 0;   
-    int yes = rocks[0];
-    int c = 1;
+ 
     int ans = 0;
-    for(int i = 1; i < rocks.size(); i++){
-        int r = rocks[i];
-        if(r==yes){
-            c++;
-        } else if (r!=yes){
-            if(max < c*yes){
-                max = c*yes;
-                ans = yes;
-            }
-            yes = r;
-            c = 1;
-        } 
-
-        if(i+1==rocks.size()){
-            if(max < c*yes){
-                max = c*yes;
-                ans = yes;
-            }
+    for(int i = 0; i < 51; i++){
+        if(i*freq[i] > max){
+            max = i*freq[i];
+            ans = i;
         }
-        
     }
 
 cout<<ans;

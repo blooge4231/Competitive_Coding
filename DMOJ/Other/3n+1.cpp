c116b09c07b76ea7c21ce2620724c68c2d256c60
blooge4231 <57263072+blooge4231@users.noreply.github.com>
2021-02-23 16:03:29 -0500

    3n+1

---
+++ DMOJ/Other/3n+1.cpp
@@ -0,0 +1,30 @@
+    #include <bits/stdc++.h>
+    using namespace std;
+
+    void solve(){
+        long long s;
+        int count = 0;
+        cin>>s;
+        while(s!=1) {
+            if(s%2 == 0) {
+                s /= 2;
+                //out<<s<<endl;
+            } else {
+                s = (s*3) + 1;
+            }
+            count++;
+        }
+
+        cout<<count<<"\n";
+
+    }
+    
+    
+    
+    int main(){
+        ios::sync_with_stdio(0);
+        //cout<<fixed<<setprecision(1);
+        cin.tie(0);
+        solve();
+        return 0;
+    }
